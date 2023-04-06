33513a28e9f677985dba6b8ae3f50b96c5bd28c6
Rokibol-Hasan <rokibul.techdynobd@gmail.com>
2023-04-06 13:47:49 +0600

    first commit

---
+++ app/Http/Controllers/Pos/SupplierController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers\Pos;
+
+use App\Http\Controllers\Controller;
+use App\Models\Supplier;
+use Illuminate\Http\Request;
+use Illuminate\Support\Carbon as SupportCarbon;
+use Illuminate\Support\Facades\Auth;
+
+class SupplierController extends Controller
+{
+
+    public function SupplierAll()
+    {
+        $suppliers = Supplier::latest()->get();
+        return view('backend.supplier.supplier_all', compact('suppliers'));
+    }
+
+
+    public function supplierAdd()
+    {
+        return view('backend.supplier.supplier_add');
+    }
+
+
+    public function supplierStore(Request $request)
+    {
+        Supplier::insert([
+            'name' => $request->name,
+            'mobile_no' => $request->mobile_no,
+            'email' => $request->email,
+            'address' => $request->address,
+            'created_by' => Auth::user()->id,
+            'created_at' => SupportCarbon::now(),
+        ]);
+
+        $notification = array(
+            'message' => 'Supplier Inserted Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('supplier.all')->with($notification);
+    }
+
+    public function supplierEdit($id){
+        $supplier = Supplier::findOrFail($id);
+        return view('backend.supplier.supplier_edit',compact('supplier'));
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ note.php
@@ -0,0 +1,2 @@
+1.start form supplier page set up --- part-4 //todo
+1.start form supplier page set up --- part-6 //todo
